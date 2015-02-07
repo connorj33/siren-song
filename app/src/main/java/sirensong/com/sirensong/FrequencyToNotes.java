29a5aebebc2f6438a65ff5d0e33d25cb5d2aa0b6
Connor Toomey <cjt51@case.edu>
2015-02-07 14:24:48 -0500

    Trashed several attempts at having note storage, decided to put the storage info in the note class

--- app/src/main/java/sirensong/com/sirensong/FrequencyToNotes.java
+++ app/src/main/java/sirensong/com/sirensong/FrequencyToNotes.java
@@ -9,29 +9,51 @@ public class FrequencyToNotes {
             "D sharp ", "E ", "F ", "F sharp ", "G ", "G sharp " };
 
 
-    private static String getNote(int frequency){
+    private static String getNoteString(int frequency) {
         int octave = 4;                                 //Beginning note in the search is A440, which is in octave 4
-        if(frequency >= 880){                           //If the note is out of the octave, we shift calculations
-            octave ++;
-            getNote(frequency / 2);
+        if (frequency >= 856) {                           //If the note is out of the octave, we shift calculations
+            octave++;
+            getNoteString(frequency / 2);
+        } else if (frequency < 440) {                        //Same as adding an octave, but if the note is too low
+            octave--;
+            getNoteString(frequency * 2);
         }
-        else if(frequency <440){                        //Same as adding an octave, but if the note is too low
-            octave --;
-            getNote(frequency * 2);
-        }
-        double curr_freq = 440;                         //Starting pitch
+        double curr_freq = 428;                         //Starting pitch
         int notesPastA = 0;
 
-        while(frequency > curr_freq){                   //Adds to the count of notes past 'A' until the frequency is too low to match the note.
+        while (frequency > curr_freq) {                   //Adds to the count of notes past 'A' until the frequency is too low to match the note.
             curr_freq = curr_freq * 1.05946;            //Also adds to octave count if 'C' is reached
             notesPastA++;
-            if(notesPastA == 4){
-                octave ++;
+            if (notesPastA == 4) {
+                octave++;
 
             }
         }
 
         //returns string in the format "C sharp 6"
         return notes[notesPastA] + Integer.toString(octave);
+
+    }
+    private static Note getNote(int frequency){
+        int octave = 4;                                 //Beginning note in the search is A440, which is in octave 4
+        if (frequency >= 856) {                           //If the note is out of the octave, we shift calculations
+            octave++;
+            getNote(frequency / 2);
+        } else if (frequency < 440) {                        //Same as adding an octave, but if the note is too low
+            octave--;
+            getNote(frequency * 2);
+        }
+        double curr_freq = 428;                         //Starting pitch
+        int notesPastA = 0;
+
+        while (frequency > curr_freq) {                   //Adds to the count of notes past 'A' until the frequency is too low to match the note.
+            curr_freq = curr_freq * 1.05946;            //Also adds to octave count if 'C' is reached
+            notesPastA++;
+            if (notesPastA == 4) {
+                octave++;
+
+            }
+        }
+        return new Note(12 * octave + notesPastA);
     }
 }
