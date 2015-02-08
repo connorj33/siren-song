4e230f7d894fcad2d140c9b55ede1cf583d01048
Stephen <lord.of.stuff@gmail.com>
2015-02-07 19:26:09 -0500

    Implemented better fft processing using the magnitude of the complex numbers rather than just the real part.

--- app/src/main/java/sirensong/com/sirensong/PeakFinder.java
+++ app/src/main/java/sirensong/com/sirensong/PeakFinder.java
@@ -9,120 +9,6 @@ import java.util.LinkedList;
  */
 public class PeakFinder {
 
-//    int scanRange = 1024;
-//    double runningTotal;
-//    double rollingAverage;
-//    double[] trimmed;
-//    double peakRatioThreshhold = 20;
-//    int peakLeft;
-//    int peakRight;
-//    LinkedList<Integer> peaks;
-//
-//
-//    public Integer[] shear(Complex[] input) {
-//        trimmed = new double[input.length];
-//        int left = 0;
-//        int right = 1;
-//        runningTotal = input[0].getReal();
-//        rollingAverage = runningTotal;
-//
-//        //expanding the range it is scanning
-//        while (right < scanRange) {
-//            runningTotal += input[right].getReal();
-//            rollingAverage = runningTotal / right;
-//            right++;
-//            trimBaseline(input, left, right, trimmed);
-//        }
-//        //move the range along
-//        while (right < input.length) {
-//            runningTotal += input[right].getReal();
-//            runningTotal -= input[left].getReal();
-//            right++;
-//            left++;
-//            rollingAverage = runningTotal / scanRange;
-//            trimBaseline(input, left, right, trimmed);
-//        }
-//        //shrink it as you get to the end
-//        while (left < input.length) {
-//            runningTotal -= input[left].getReal();
-//            rollingAverage = runningTotal / input.length - left;
-//            trimBaseline(input, left, right, trimmed);
-//        }
-//
-//        //at this point, you have the baseline trimmed out, you can do another pass and find ratios.
-//        //peaks should be much more obvious.
-//        left = 0;
-//        right = 1;
-//        peakLeft = -1;
-//        peakRight = -1;
-//        runningTotal = trimmed[0];
-//        rollingAverage = runningTotal;
-//        peaks = new LinkedList<>();
-//
-//        while (right < scanRange) {
-//            runningTotal += trimmed[right];
-//            rollingAverage = runningTotal / right;
-//            right++;
-//            findPeak(left, right, trimmed);
-//        }
-//        //move the range along
-//        while (right < input.length) {
-//            runningTotal += trimmed[right];
-//            runningTotal -= trimmed[left];
-//            right++;
-//            left++;
-//            rollingAverage = runningTotal / scanRange;
-//            findPeak(left, right, trimmed);
-//        }
-//        //shrink it as you get to the end
-//        while (left < input.length) {
-//            runningTotal -= trimmed[left];
-//            rollingAverage = runningTotal / input.length - left;
-//            findPeak(left, right, trimmed);
-//        }
-//
-//
-//
-//        return peaks.toArray(new Integer[0]);
-//    }
-//
-//    void trimBaseline(Complex[] input, int left, int right, double[] trimmed) {
-//        int position = (right+left)/2;
-//        double current = input[position].getReal();
-////        double ratio = current/rollingAverage;
-////        if (ratio > 2)
-//        if (current < rollingAverage) {
-//            trimmed[position] = 0;
-//        }
-//        else {
-//            trimmed[position] = input[position].getReal() - rollingAverage;
-//        }
-//    }
-//
-//    void findPeak(int left, int right, double[] trimmed) {
-//        int position = (right+left)/2;
-//        double current = trimmed[position];
-//        double ratio = current/rollingAverage;
-//        if (ratio > peakRatioThreshhold) {
-//            if (peakLeft == -1) { //not already detecting a peak, start detecting one
-//                //peaking = true;
-//                peakLeft = position;
-//                peakRight = position;
-//            }
-//            else { //widen the detected peak
-//                peakRight = position;
-//            }
-//        }
-//        else {
-//            //check if we have just left a peak
-//            if (peakRight != 0) {
-//                peaks.add((peakRight + peakLeft) / 2);
-//                peakLeft = -1;
-//                peakRight = -1;
-//            }
-//        }
-//    }
-
     double posThresh = 20000000;
     double negThresh = -20000000;
 
@@ -136,7 +22,7 @@ public class PeakFinder {
     public Integer[] findPeaks(Complex[] fft) {
         LinkedList<Integer> peaks = new LinkedList<>();
         for (int i = HighPass; i < fft.length /2; i++) {
-            if (fft[i].getReal() > posThresh || fft[i].getReal() < negThresh) {
+            if (fft[i].abs() > posThresh ) {
                 if (peakLeft == -1) { //not already detecting a peak, start detecting one
                     peakLeft = i;
                     peakRight = i;
@@ -162,6 +48,5 @@ public class PeakFinder {
         }
         return peaks.toArray(new Integer[0]);
     }
-
 
 }
