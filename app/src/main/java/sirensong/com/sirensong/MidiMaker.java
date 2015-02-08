bff2dc3839404f3eac3812c4d9a4b0ab98ca6ce4
Connor Toomey <cjt51@case.edu>
2015-02-08 09:00:10 -0500

    same as before

--- app/src/main/java/sirensong/com/sirensong/MidiMaker.java
+++ app/src/main/java/sirensong/com/sirensong/MidiMaker.java
@@ -52,7 +52,7 @@ public class MidiMaker{
                 for (int j = 0; j < bessie.noteList[i].times.size(); j++) {
                     pitch = bessie.noteList[i].getPitch();
                     if(!(bessie.noteList[i].durations.isEmpty())) {
-                        noteTrack.insertNote(channel, pitch, velocity, 50, bessie.noteList[i].durations.element());
+                        noteTrack.insertNote(channel, pitch, velocity, (System.currentTimeMillis()-baseTick)*300, bessie.noteList[i].durations.element());
                         durations.add(bessie.noteList[i].durations.element());
                     }
                 }
